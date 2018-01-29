988beb51bbb22b935925e4d4d6556f5a2dfc3ef5
liefu zhang <liefuzhangnz@gmail.com>
2018-01-29 19:18:04 +1300

    added moq

--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
 using NUnit.Framework.Internal;
 using NUnit.Framework;
 using TestNinja.Mocking;
@@ -12,7 +13,10 @@ namespace TestNinja.UnitTests.Mocking {
     public class VideoServiceTests {
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnError() {
-            var service = new VideoService(new FakeFileReader());
+            var fileReader = new Mock<IFileReader>();
+            fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
+
+            var service = new VideoService(fileReader.Object);
 
             var result = service.ReadVideoTitle();
 
