175014c24bf2ec581ba9e2de97af998dfeab1bd6
Liefu Zhang <liefu.zhang@aderant.com>
2018-01-31 18:11:32 +1300

    temp save

---
+++ TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework.Internal;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking {
+    [TestFixture]
+    public class BookingHelperTests {
+        private Mock<IUnitOfWork> _unitOfWork;
+        private Mock<IBookingRepository> _bookingRepository;
+
+        [SetUp]
+        public void Setup() {
+            _unitOfWork = new Mock<IUnitOfWork>();
+            _bookingRepository = new Mock<IBookingRepository>();
+            _unitOfWork.Object.BookingRepository = _bookingRepository.Object;
+            BookingHelper.UnitOfWork = _unitOfWork.Object;
+        }
+
+        [Test]
+        public void OverlappingBookingsExist_BookingStatusIsCancelled_ReturnEmptyString() {
+            var booking = new Booking { Status = "Cancelled" };
+
+            var result = BookingHelper.OverlappingBookingsExist(booking);
+
+            Assert.That(result, Is.EqualTo(""));
+        }
+
+        [Test]
+        public void OverlappingBookingsExist_NoOverlappingBooking_ReturnEmptyString() {
+            var booking = new Booking();
+
+            _bookingRepository.Setup(br => br.GetOverlappingBooking(booking)).Returns((Booking) null);
+            
+            var result = BookingHelper.OverlappingBookingsExist(booking);
+
+            Assert.That(result, Is.EqualTo(""));
+        }
+
+    }
+}
---
+++ TestNinja/Mocking/BookingRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.Mocking {
+    public interface IBookingRepository {
+        Booking GetOverlappingBooking(Booking booking);
+    }
+
+    public class BookingRepository : IBookingRepository {
+        private IUnitOfWork _unitOfWork;
+
+        public BookingRepository(IUnitOfWork unitOfWork) {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Booking GetOverlappingBooking(Booking booking) {
+            var bookings = _unitOfWork.Query<Booking>()
+                    .Where(b => b.Id != booking.Id && b.Status != "Cancelled");
+
+            return bookings.FirstOrDefault(
+                    b => booking.ArrivalDate >= b.ArrivalDate
+                        && booking.ArrivalDate < b.DepartureDate
+                        || booking.DepartureDate > b.ArrivalDate
+                        && booking.DepartureDate <= b.DepartureDate);
+        }
+    }
+}
