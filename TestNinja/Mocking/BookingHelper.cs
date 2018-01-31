5e420b399df3006b2dfc166dd81af6204737ecbd
Liefu Zhang <liefu.zhang@aderant.com>
2018-01-31 20:42:31 +1300

    temp save

--- TestNinja/Mocking/BookingHelper.cs
+++ TestNinja/Mocking/BookingHelper.cs
@@ -2,43 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace TestNinja.Mocking
-{
-    public static class BookingHelper
-    {
-        public static string OverlappingBookingsExist(Booking booking)
-        {
+namespace TestNinja.Mocking {
+    public static class BookingHelper {
+        public static IUnitOfWork UnitOfWork;
+
+        static BookingHelper() {
+            UnitOfWork = new UnitOfWork();
+        }
+
+        public static string OverlappingBookingsExist(Booking booking) {
             if (booking.Status == "Cancelled")
                 return string.Empty;
 
-            var unitOfWork = new UnitOfWork();
-            var bookings =
-                unitOfWork.Query<Booking>()
-                    .Where(
-                        b => b.Id != booking.Id && b.Status != "Cancelled");
-
-            var overlappingBooking =
-                bookings.FirstOrDefault(
-                    b =>
-                        booking.ArrivalDate >= b.ArrivalDate
-                        && booking.ArrivalDate < b.DepartureDate
-                        || booking.DepartureDate > b.ArrivalDate
-                        && booking.DepartureDate <= b.DepartureDate);
+            var overlappingBooking = UnitOfWork.BookingRepository.GetOverlappingBooking(booking);
 
             return overlappingBooking == null ? string.Empty : overlappingBooking.Reference;
         }
     }
 
-    public class UnitOfWork
-    {
-        public IQueryable<T> Query<T>()
-        {
-            return new List<T>().AsQueryable();
-        }
+    public interface IUnitOfWork {
+        IQueryable<T> Query<T>();
+        IBookingRepository BookingRepository { get; set; }
     }
 
-    public class Booking
-    {
+    public class UnitOfWork : IUnitOfWork {
+        public UnitOfWork() {
+            BookingRepository = new BookingRepository(this);
+        }
+        public IBookingRepository BookingRepository { get; set; }
+
+        public IQueryable<T> Query<T>() {
+            return new List<T>().AsQueryable();
+        }
+
+    }
+
+    public class Booking {
         public string Status { get; set; }
         public int Id { get; set; }
         public DateTime ArrivalDate { get; set; }
